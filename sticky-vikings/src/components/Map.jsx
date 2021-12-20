af6d2ad54116979095a8d96a3531d28bc401ea2f
SapirEgozi <sapir.egozi@optibus.com>
2021-12-20 18:01:33 +0200

    add search logic

--- sticky-vikings/src/components/Map.jsx
+++ sticky-vikings/src/components/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React, { useState, useCallback } from "react";
 import {
   GoogleMap,
   useJsApiLoader,
@@ -8,7 +8,7 @@ import {
 import Search from "./Search";
 
 const containerStyle = {
-  width: "800px",
+  width: "100%",
   height: "800px",
 };
 
@@ -16,6 +16,87 @@ const center = {
   lat: 32.0929,
   lng: 34.807,
 };
+
+const theme = [
+  { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [{ color: "#263c3f" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#6b9a76" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [{ color: "#38414e" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#212a37" }],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#9ca5b3" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [{ color: "#746855" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#1f2835" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#f3d19c" }],
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [{ color: "#2f3948" }],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [{ color: "#17263c" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#515c6d" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.stroke",
+    stylers: [{ color: "#17263c" }],
+  },
+]
 
 let c = 1;
 
@@ -27,8 +108,11 @@ function Map() {
 
   const [map, setMap] = useState(null);
   const [route, setRoute] = useState(null);
-  const [destination, setDestination] = useState("Haifa");
-  const [origin, setOrigin] = useState("שלמה אבן גבירול, Tel Aviv-Yafo");
+  const [options, setOptions] = useState({
+    destination: "",
+    origin: "",
+    travelMode: "TRANSIT",
+  });
   const onLoad = useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds();
     map.fitBounds(bounds);
@@ -39,53 +123,52 @@ function Map() {
     setMap(null);
   }, []);
 
-  const options = useMemo(
-    () => ({
-      destination,
-      origin,
-      travelMode: "TRANSIT",
-    }),
-    [destination, origin]
-  );
+  const onSearch = (origin, destination) => {
+    setOptions((prevState) => ({ ...prevState, origin, destination }));
+  };
 
   const onResponse = useCallback(
     (res) => {
-      console.log({ destination, origin, res });
       c && !route && setRoute(res);
     },
     [route]
   );
   return isLoaded ? (
     <>
-      <Search />
+      <Search onSearch={onSearch} />
       <GoogleMap
         mapContainerStyle={containerStyle}
+        options={{
+          styles: theme,
+        }}
         center={center}
         zoom={10}
         onLoad={onLoad}
         onUnmount={onUnmount}
       >
         {/* Child components, such as markers, info windows, etc. */}
-        <DirectionsService
-          // required
-          options={options}
-          // required
-          callback={onResponse}
-          // optional
-          onLoad={(directionsService) => {
-            console.log(
-              "DirectionsService onLoad directionsService: ",
-              directionsService
-            );
-          }}
-          // optional
-          onUnmount={(directionsService) => {
-            console.log(
-              "DirectionsService onUnmount directionsService: ",
-              directionsService
-            );
-          }}
-        />
+        {options.destination && options.origin && (
+          <DirectionsService
+            // required
+            options={options}
+            // required
+            callback={onResponse}
+            // optional
+            onLoad={(directionsService) => {
+              console.log(
+                "DirectionsService onLoad directionsService: ",
+                directionsService
+              );
+            }}
+            // optional
+            onUnmount={(directionsService) => {
+              console.log(
+                "DirectionsService onUnmount directionsService: ",
+                directionsService
+              );
+            }}
+          />
+        )}
 
         {route && (
           <DirectionsRenderer
--- sticky-vikings/src/components/Search.jsx
+++ sticky-vikings/src/components/Search.jsx
@@ -1,42 +1,78 @@
 import React, { useState } from "react";
-import { Divider, IconButton, InputBase, Paper } from "@mui/material";
+import {
+  Divider,
+  IconButton,
+  InputBase as MuiInput,
+  Paper as MuiPaper,
+  Stack as MuiStack,
+  styled,
+} from "@mui/material";
 import DirectionsIcon from "@mui/icons-material/Directions";
-import SearchIcon from "@mui/icons-material/Search";
+
+const Stack = styled(MuiStack)({
+  paddingTop: "60px",
+  width: "100%",
+  zIndex: 10,
+  position: "fixed",
+});
+
+const Paper = styled(MuiPaper)({
+  backgroundColor: "#344152"
+});
+
+
+const InputBase = styled(MuiInput)({
+  color: "#fff",
+  fontWeight: "bold"
+});
 
 const Search = ({ onSearch: propsOnSearch }) => {
-  const [destination, setDestination] = useState();
-  const [origin, setOrigin] = useState("Tel Aviv-Yafo");
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
 
-  const onSearch = (e) => {
-    propsOnSearch();
+  const onSearch = () => {
+    propsOnSearch(origin, destination);
   };
 
   return (
-    <Paper
-      component="form"
-      sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
-    >
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search Destination"
-        value={destination}
-        onChange={(e) => {
-          setDestination(e.target.value);
+    <Stack direction="column" justifyContent="center" alignItems="center">
+      <Paper
+        sx={{
+          p: "2px 4px",
+          display: "flex",
+          alignItems: "center",
+          width: "92%",
         }}
-      />
-      <IconButton
-        type="submit"
-        onClick={onSearch}
-        sx={{ p: "10px" }}
-        aria-label="search"
       >
-        <SearchIcon />
-      </IconButton>
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton color="primary" sx={{ p: "10px" }} aria-label="directions">
-        <DirectionsIcon />
-      </IconButton>
-    </Paper>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Origin"
+          value={origin}
+          onChange={(e) => {
+            setOrigin(e.target.value);
+          }}
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Destination"
+          value={destination}
+          onChange={(e) => {
+            setDestination(e.target.value);
+          }}
+        />
+
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton
+          onClick={onSearch}
+          color="primary"
+          sx={{ p: "10px" }}
+          aria-label="directions"
+        >
+          <DirectionsIcon />
+        </IconButton>
+      </Paper>
+    </Stack>
   );
 };
 
